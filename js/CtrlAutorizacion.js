283424400e57b7424cf4976fc6982a6696379dce
grupweb2000 <grupweb2000@gmail.com>
2021-05-25 15:18:07 -0500

    Cambios nuevos

--- js/CtrlAutorizacion.js
+++ js/CtrlAutorizacion.js
@@ -1,31 +1,14 @@
 "use strict";
 var forma = document.getElementById("forma"),
-        salidaBizcochos = document.getElementById("salidaBizcochos"),
-        salidaCelebridades = document.getElementById("salidaCelebridades"),
-        bizcochos = forma["bizcochos"],
-        celebridades = document.getElementsByName("celebridades");
-forma.addEventListener("change", validaBizcochos, false);
-forma.addEventListener("click", validaCelebridades, false);
-validaBizcochos();
-validaCelebridades();
-function validaBizcochos() {
-  var opciones = bizcochos.options, bizcochosSeleccionados = new Array(); 
-  for (var i = 0, total = opciones.length; i < total; i++) {
-    var bizcocho = opciones[i];
-    if (bizcocho.selected){
-      bizcochosSeleccionados.push(bizcocho.value);
-    }
+campos = ["nombre", "secreto", "navegador", "email", "usuario","url", "telefono", "verso"],
+salidas = ["salidaNombre", "salidaSecreto", "salidaNavegador", "salidaEmail", "salidaUsuario",
+          "salidaUrl", "salidaTelefono", "salidaVerso"];
+
+forma.addEventListener("submit", formularios, false);
+function formularios(){
+  for (var i = 0, longitud = campos.length; i < longitud; i++) {
+    var campo = forma[campos[i]];
+    var salida = document.getElementById(salidas[i]);
+    salida.value = campo.value;
   }
-  salidaBizcochos.textContent = bizcochosSeleccionados.join(", ");
-}
-function validaCelebridades() {
-  var celebridadesSeleccionadas = new Array(); 
-  for (var i = 0, total = celebridades.length; i < total; i++) {
-    var celebridad = celebridades[i];
-    if (celebridad.checked){
-      celebridadesSeleccionadas.push(celebridad.value);
-    }
-  }
-  salidaCelebridades.textContent = celebridadesSeleccionadas.join(", ") + "."
-  + (celebridadesSeleccionadas.length === 3 ? "Autorizado" : "No Autorizado");
 }
